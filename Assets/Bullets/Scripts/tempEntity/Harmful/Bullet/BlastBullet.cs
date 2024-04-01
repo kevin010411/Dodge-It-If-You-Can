3db57e07bb1537bc8e3920a967defa762fb02aa4
Quib <steven911221@gmail.com>
2024-04-01 23:44:57 +0800

    Blast TODO

---
+++ Assets/Bullets/Scripts/tempEntity/Harmful/Bullet/BlastBullet.cs
@@ -0,0 +1,12 @@
+// TODO
+// inherit  DurationBullet , ADD Collide detection , once func is triggered
+// generate new DurationBullet every 45 degree
+using System;
+using Unity;
+
+// Name                 Feature          
+// BlastBullet          碰到牆壁就生成8顆新的DurationBullet
+public class BlastBullet : DurationBullet
+{
+
+}
